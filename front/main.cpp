99db3451dcc30a2e5e86a960ae3cdb2ca7896129
LABOIRIE Alex <alex.laboirie@enseirb.fr>
2023-03-17 11:58:26 +0100

    better smileyy [UI]

--- front/main.cpp
+++ front/main.cpp
@@ -2,15 +2,21 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::RenderWindow window(sf::VideoMode(2500, 1900), "SFML works");
 
     sf::Texture texture;
-    if (!texture.loadFromFile("../assets/smiley.jpeg")) {
+    if (!texture.loadFromFile("../assets/angry.png")) {
         printf("File not loaded");
     }
 
     sf::Sprite sprite(texture);
+    sprite.setScale(0.1f, 0.1f);
 
+
+
+    sf::Clock clock;
+    float speed = 100.0f;
+    int direction = 1;
     while (window.isOpen())
     {
         sf::Event event;
@@ -20,6 +26,9 @@ int main()
             if (event.type == sf::Event::Closed)
                 window.close();
         }
+
+        float elapsedTime = clock.restart().asSeconds();
+        sprite.move(speed * elapsedTime, speed * elapsedTime); // Move the sprite horizontally
 
         window.clear();
         window.draw(sprite);
