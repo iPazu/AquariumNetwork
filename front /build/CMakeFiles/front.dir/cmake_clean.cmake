37725d3581ad8f636fb621cdf7bef482b84ee19f
Nicolas Gry <grybouilli@outlook.fr>
2023-03-17 11:03:22 +0100

    [UI] added cmake base to build sfml projects

---
+++ front /build/CMakeFiles/front.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/front.dir/main.cpp.o"
+  "CMakeFiles/front.dir/main.cpp.o.d"
+  "front"
+  "front.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/front.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ front /build/CMakeFiles/front.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for front.
+# This may be replaced when dependencies are built.
