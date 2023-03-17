f6c70497d2ae20402a00930a2979c9ecbd8d7e0f
LABOIRIE Alex <alex.laboirie@enseirb.fr>
2023-03-17 11:37:46 +0100

    Now there is a smiley :) [UI]

--- front/main.cpp
+++ front/main.cpp
@@ -3,20 +3,27 @@
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+
+    sf::Texture texture;
+    if (!texture.loadFromFile("../assets/smiley.jpeg")) {
+        printf("File not loaded");
+    }
+
+    sf::Sprite sprite(texture);
 
     while (window.isOpen())
     {
         sf::Event event;
         while (window.pollEvent(event))
         {
+
             if (event.type == sf::Event::Closed)
                 window.close();
         }
 
         window.clear();
-        window.draw(shape);
+        window.draw(sprite);
+
         window.display();
     }
 
