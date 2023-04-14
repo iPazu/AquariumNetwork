7ec5a873a48fc3718fd43ba5dae0d41abe5dc781
PETIT Axel <axel.petit@enseirb.fr>
2023-04-14 08:24:40 +0200

    changing function view_init

--- src/Modele/view.c
+++ src/Modele/view.c
@@ -7,6 +7,14 @@
 #include "view.h"
 
 view *init_view(int id, int x, int y, int width, int height) {
+    if (width <= 0 || height <= 0) {
+        printf("Error: width and height must be positive\n");
+        return NULL;
+    }
+    if (x < 0 || y < 0) {
+        printf("Error: x and y must be positive\n");
+        return NULL;
+    }
     view *v = malloc(sizeof(view));
     v->id = id;
     v->x = x;
