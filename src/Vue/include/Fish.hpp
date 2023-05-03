03a63a86bdacc6969f4004f63fcf6f8cfe1154ad
Nicolas Gry <grybouilli@outlook.fr>
2023-05-03 19:06:32 +0200

    [UI] restructured fish code

--- src/Vue/include/Fish.hpp
+++ src/Vue/include/Fish.hpp
@@ -21,64 +21,16 @@ class Fish : public SpriteNode
 {
 public:
 
-    explicit Fish(FISH_TYPE FishType) 
-    : SpriteNode {FishSpriteProperties[(int)FishType]}
-    , mTrajectory{ TrajectoryFactory::staticTrajectory(0,0)}
-    , mElapsedTime{sf::Time::Zero}
-    , mTargetTime {sf::seconds(0.f)}
-    , mDirection { -1, 0 }
-    {
+    explicit Fish(FISH_TYPE FishType);
 
-    }
+    Fish(float posx, float posy, float targetx, float targety,float timeToTarget, FISH_TYPE FishType, FISH_BEHAVIOR FishBehavior);
 
-    Fish(float posx, float posy, float targetx, float targety,float timeToTarget, FISH_TYPE FishType, FISH_BEHAVIOR FishBehavior)
-    : SpriteNode {FishSpriteProperties[(int)FishType]}
-    , mTrajectory { TrajectoryFactory::makeTrajectory(posx, posy, targetx, targety, timeToTarget, FishBehavior) }
-    , mElapsedTime{sf::Time::Zero}
-    , mTargetTime { sf::seconds(timeToTarget) }
-    , mDirection { -1, 0 }
-    {
-        setPosition(posx, posy);
-    }
+    virtual void updateCurrent(sf::Time dt);
 
-    virtual void updateCurrent(sf::Time dt) 
-    {
-        mElapsedTime += dt;
-        if(mElapsedTime < mTargetTime)
-        {
-            auto targetPosition = mTrajectory(mElapsedTime);
-            updateDirection(targetPosition);
-            setPosition(targetPosition);
-        }
-        animate(dt);
-
-    }
-
-    void renewTarget(float targetx, float targety, float time, FISH_BEHAVIOR FishBehavior = FISH_BEHAVIOR::LINEAR)
-    {
-        mTargetTime = sf::seconds(time);
-        mElapsedTime = sf::Time::Zero;
-        sf::Vector2f pos = getPosition();
-        mTrajectory = TrajectoryFactory::makeTrajectory(pos.x, pos.y, targetx, targety, time, FishBehavior);
-    }
+    void renewTarget(float targetx, float targety, float time, FISH_BEHAVIOR FishBehavior = FISH_BEHAVIOR::LINEAR);
 
 private:
-    void updateDirection(const sf::Vector2f& targetPosition)
-    {
-        auto previousDirection = mDirection;
-        mDirection = normalize(targetPosition - getPosition());
-        float angle = toDegrees(vectorsAngle(previousDirection, mDirection));
-        rotate(angle);
-
-        if(mDirection.x > 0 && getScale().y > 0)
-        {
-
-            setScale(getScale().x,-getScale().y);
-        } else if (mDirection.x <= 0 && getScale().y < 0)
-        {
-            setScale(-getScale().x,getScale().y);
-        }
-    }
+    void updateDirection(const sf::Vector2f& targetPosition);
 private:
     Trajectory    mTrajectory;
     sf::Time      mElapsedTime;
